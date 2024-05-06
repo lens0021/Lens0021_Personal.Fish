9c18ded1992e99781667db9dadc6bd80c44eaca3
lens0021 <lorentz0021@gmail.com>
2024-05-06 16:56:14 +0900

    Add more environment variables
    Imported from
    https://gitlab.com/lens0021/provision/-/commit/ba5d89a81f9d40fbf6270a74e3902a0edcf39d0b

--- src/conf.d/envs.fish
+++ src/conf.d/envs.fish
@@ -1,3 +1,7 @@
 if status is-interactive
-    set -q LESS || set -gx LESS '--mouse --wheel-lines=3'
+    set -q LESS || set -x LESS '--mouse --wheel-lines=3'
+    set -x EDITOR hx
+    set -x GPG_TTY (tty)
+    set -x KUBE_EDITOR hx
+    set -x PAGER 'less -r'
 end
