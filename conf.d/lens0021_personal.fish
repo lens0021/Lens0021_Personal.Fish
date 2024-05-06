95cfba195114c13c6bc04726d07cd74bd31acf85
lens0021 <lorentz0021@gmail.com>
2024-05-06 16:57:10 +0900

    Build

--- conf.d/lens0021_personal.fish
+++ conf.d/lens0021_personal.fish
@@ -28,7 +28,11 @@ rm temp.go\'
 
 end
 if status is-interactive
-    set -q LESS || set -gx LESS '--mouse --wheel-lines=3'
+    set -q LESS || set -x LESS '--mouse --wheel-lines=3'
+    set -x EDITOR hx
+    set -x GPG_TTY (tty)
+    set -x KUBE_EDITOR hx
+    set -x PAGER 'less -r'
 end
 if status is-login
   set -gx PATH $PATH $HOME/.local/bin
