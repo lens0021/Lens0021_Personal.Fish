841eacd54e9c56039fb476c7dcc6dcf376b910ef
lens0021 <lorentz0021@gmail.com>
2024-05-06 17:03:00 +0900

    Add more tool configs
    Imported from https://gitlab.com/lens0021/provision/-/commit/ba5d89a81f9d40fbf6270a74e3902a0edcf39d0b

--- conf.d/lens0021_personal.fish
+++ conf.d/lens0021_personal.fish
@@ -46,7 +46,25 @@ if status is-login
   end
 end
 if status is-interactive
-  if set -q TERMUX_VERSION
-    tide configure --auto --style=Lean --prompt_colors='True color' --show_time='24-hour format' --lean_prompt_height='Two lines' --prompt_connection=Disconnected --prompt_spacing=Compact --icons='Many icons' --transient=No
+
+  if functions -q fzf_configure_bindings
+    # Change key bindings for fzf.fish
+    # https://github.com/PatrickF1/fzf.fish/blob/main/functions/fzf_configure_bindings.fish
+    fzf_configure_bindings --variables= --directory=\cv --history=\e\cr
   end
+
+  if test -e ~/.asdf/asdf.fish
+      source ~/.asdf/asdf.fish
+  end
+
+  if functions -q declair
+      declair update_rc
+  end
+
+  if set -q TERMUX_VERSION
+    # if function -q tide
+    #   tide configure --auto --style=Lean --prompt_colors='True color' --show_time='24-hour format' --lean_prompt_height='Two lines' --prompt_connection=Disconnected --prompt_spacing=Compact --icons='Many icons' --transient=No
+    # end
+  end
+
 end
--- src/conf.d/tide.fish
+++
@@ -1,5 +0,0 @@
-if status is-interactive
-  if set -q TERMUX_VERSION
-    tide configure --auto --style=Lean --prompt_colors='True color' --show_time='24-hour format' --lean_prompt_height='Two lines' --prompt_connection=Disconnected --prompt_spacing=Compact --icons='Many icons' --transient=No
-  end
-end
