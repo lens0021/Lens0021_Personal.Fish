f3a77dfb203f24b7ce4da18cef63a6860bddcd7a
lens0021 <lorentz0021@gmail.com>
2024-05-11 13:52:49 +0000

    Use shorten name

--- src/functions/install.fish
+++ src/functions/install.fish
@@ -1,14 +1,14 @@
-function _lens0021_install
+function _lens_install
     argparse --stop-nonopt -- $argv
 
-    if functions --query _lens0021_install_$argv[1]
-        _lens0021_install_$argv[1] $argv[2..]
+    if functions --query _lens_install_$argv[1]
+        _lens_install_$argv[1] $argv[2..]
     else
         echo no $argv[1]
     end
 end
 
-function _lens0021_install_saml2aws
+function _lens_install_saml2aws
     set -l ver 2.36.15
     set -l arch arm64
     curl -L "https://github.com/Versent/saml2aws/releases/download/v$ver/saml2aws_""$ver""_linux_$arch.tar.gz" ~/Downloads/saml2aws.tar.gz
