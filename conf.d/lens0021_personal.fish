e9c8231308424343d47ca5c88f7a176b71e94b15
lens0021 <lorentz0021@gmail.com>
2024-06-11 11:50:43 +0900

    build

--- conf.d/lens0021_personal.fish
+++ conf.d/lens0021_personal.fish
@@ -36,6 +36,8 @@ if status is-interactive
     set -q LESS || set -x LESS '--mouse --wheel-lines=3'
     set -x EDITOR hx
     set -x GPG_TTY (tty)
+    set -x HELM_DIFF_COLOR true
+    set -x HELM_DIFF_TPL dyff
     set -x KUBE_EDITOR hx
     set -x PAGER 'less -rXF'
 end
