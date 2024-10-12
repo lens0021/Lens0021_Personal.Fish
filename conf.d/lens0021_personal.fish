84bb652a469e8d1b8203f67af37cdb3945548d36
lens0021 <lorentz0021@gmail.com>
2024-10-12 16:54:41 +0900

    Add fw-aws-soo abbr

--- conf.d/lens0021_personal.fish
+++ conf.d/lens0021_personal.fish
@@ -20,6 +20,8 @@ if status is-interactive
     abbr -a -- gitgraphfzf "git log --graph --all --decorate --oneline --color | fzf --multi --tiebreak=index --layout reverse --ansi --no-sort --preview 'echo {} | rg --only-matching --max-count 1 '[0-9a-f]\\\\{7\\\\}' | xargs -I % sh -c \'git show --color=always \\%\'' | rg '[0-9a-f]{7}' --only-matching --max-count 1"
 
     abbr -a --set-cursor -- gotemp 'bash -c \'cat <<EOF > temp.go
+
+    abbr -a -- fw-aws-sso 'aws --profile fw configure sso'
 package main
 
 import "fmt"
