a298c580715a925dfb6e818dacf83d36219a7c26
lens0021 <lorentz0021@gmail.com>
2024-10-29 11:40:48 +0900

    Add Y to yazi as sidebar

--- src/conf.d/tools.fish
+++ src/conf.d/tools.fish
@@ -30,4 +30,13 @@ if status is-interactive
         rm -f -- "$tmp"
     end
 
+    function Y
+        set tmp (mktemp -t "yazi-cwd.XXXXXX")
+        YAZI_CONFIG_HOME=~/.config/abyazelix/yazi/sidebar yazi $argv --cwd-file="$tmp"
+        if set cwd (command cat -- "$tmp"); and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+            builtin cd -- "$cwd"
+        end
+        rm -f -- "$tmp"
+    end
+
 end
