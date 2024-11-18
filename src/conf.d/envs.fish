45fefccdb95f89ffd9a15a574fd1ee40683a486e
lens0021 <lorentz0021@gmail.com>
2024-11-18 16:25:07 +0900

    Set HELM_DIFF_USE_UPGRADE_DRY_RUN true

--- src/conf.d/envs.fish
+++ src/conf.d/envs.fish
@@ -4,6 +4,7 @@ if status is-interactive
     set -x GPG_TTY (tty)
     set -x HELM_DIFF_COLOR true
     set -x HELM_DIFF_OUTPUT dyff
+    set -x HELM_DIFF_USE_UPGRADE_DRY_RUN true
     set -x KUBE_EDITOR hx
     set -x PAGER 'less -rXF'
 end
