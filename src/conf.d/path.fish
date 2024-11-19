56085ba9f4275959a1c735452b01c6c1e87ce918
lens0021 <lorentz0021@gmail.com>
2024-11-19 11:13:01 +0900

    PATH: Add bun

--- src/conf.d/path.fish
+++ src/conf.d/path.fish
@@ -4,6 +4,7 @@ if status is-login
     set -gx PATH $PATH $HOME/.composer/vendor/bin
     set -gx PATH $PATH $HOME/bin
     set -gx PATH $PATH $HOME/.cargo/bin/
+    set -gx PATH $PATH $HOME/.bun/bin
     if set -q KREW_ROOT
         set -gx PATH $PATH $KREW_ROOT/bin
     else if test -d $HOME/.krew/bin
