926eaf24aa24b62a37c99363ee81cc82a01864d2
lens0021 <lorentz0021@gmail.com>
2025-01-14 11:05:38 +0900

    Use fish cd
    I want the directory stack

--- conf.d/lens0021_personal.fish
+++ conf.d/lens0021_personal.fish
@@ -101,7 +101,7 @@ if status is-interactive
         set tmp (mktemp -t "yazi-cwd.XXXXXX")
         yazi $argv --cwd-file="$tmp"
         if set cwd (command cat -- "$tmp"); and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
-            builtin cd -- "$cwd"
+            cd -- "$cwd"
         end
         rm -f -- "$tmp"
     end
