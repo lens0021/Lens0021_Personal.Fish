f2ecd43493fde5464ff2f7c7e747f77c3a0988b7
lens0021 <lorentz0021@gmail.com>
2025-02-13 17:51:15 +0900

    Set TF_PLUGIN_CACHE_DIR

--- conf.d/lens0021_personal.fish
+++ conf.d/lens0021_personal.fish
@@ -56,6 +56,7 @@ if status is-interactive
     set -x HELM_DIFF_USE_UPGRADE_DRY_RUN true
     set -x KUBE_EDITOR hx
     set -x PAGER 'less -rXF'
+    set -x TF_PLUGIN_CACHE_DIR "$HOME/.terraform.d/plugin-cache"
 end
 if status is-login
     set -gx PATH $PATH $HOME/.local/bin
