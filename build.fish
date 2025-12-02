52faa7bbeea6c5f88662883920da81c2aa536a1f
lens0021 <lorentz0021@gmail.com>
2025-12-02 16:17:30 +0900

    Build completions

--- build.fish
+++ build.fish
@@ -4,3 +4,4 @@ fish_indent -w **/*.fish
 
 cat src/conf.d/*.fish >conf.d/$plugin_name.fish
 cat src/functions/*.fish >functions/$plugin_name.fish
+cat src/completions/*.fish >completions/$plugin_name.fish
