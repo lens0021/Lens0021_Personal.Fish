c60982870fcd42f2fc68dc65809fbcce39aa21cc
lens0021 <lorentz0021@gmail.com>
2025-12-06 06:17:43 +0900

    bind alt-backspace backward-kill-path-component

--- conf.d/lens0021_personal.fish
+++ conf.d/lens0021_personal.fish
@@ -45,6 +45,7 @@ end
 if status is-interactive
 
     bind --mode default \eL lazygit
+    bind alt-backspace backward-kill-path-component
 
 end
 if status is-interactive
--- src/conf.d/bind.fish
+++ src/conf.d/bind.fish
@@ -1,5 +1,6 @@
 if status is-interactive
 
     bind --mode default \eL lazygit
+    bind alt-backspace backward-kill-path-component
 
 end
